2ef4afbcac51d3700e42992c4602e11bab14bce9
Giu Roperto <giu.roperto@gmail.com>
2021-05-11 23:50:45 -0300

    adjusting footer styling + links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
+
 import './Footer.css';
+import ROUTES from '../constants/routes';
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <Link>
+      <Link to={ROUTES.ORIONE}>
         <div>ORIONE</div>
       </Link>
-      <Link>
+      <Link to={ROUTES.ACHIROPITA}>
         <div>ACHIROPITA</div>
       </Link>
-      <Link>
+      <Link to={ROUTES.BIXIGA}>
         <div>BIXIGA</div>
       </Link>
     </div>
