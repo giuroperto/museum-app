135d4930c5cb84926de7b6c5103500248dd777b4
Giu Roperto <giu.roperto@gmail.com>
2021-07-04 20:31:45 -0300

    adjusting navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,13 +10,15 @@ import { HistoryContext } from '../../Utils/Context/';
 const Navbar = (props) => {
 
   console.log(props);
-  console.log(props.history.goBack());
+  // console.log(props.history.goBack());
 
   const historyData = useContext(HistoryContext);
 
   const [ showBackButton, setShowBackButton ] = useState("back-hide");
-  const [ lastPage, setLastPage ] = useState("");
+  // const [ lastPage, setLastPage ] = useState("");
+
   const history = useHistory();
+
   // const [ history, setHistory ] = useState(props.history);
   // const [ page, setPage ] = useState("");
 
@@ -29,45 +31,51 @@ const Navbar = (props) => {
   }, [historyData]);
 
   useEffect(() => {
-    if (historyData.historyArray) {
-      checkPage();
+    if (history.location.pathname) {
+      
     }
-  }, [historyData]);
+  }, [history]);
+
+  // useEffect(() => {
+  //   if (historyData.historyArray) {
+  //     checkPage();
+  //   }
+  // }, [historyData]);
 
   // useEffect(() => {
   //   if ()
   // }, [])
 
-  const checkPage = () => {
-    console.log("checkpage");
-    console.log("history" + historyData);
-    console.log("history lenght" + historyData.historyArray.length);
-    console.log("inside checkpage if");
-    let historyArray = historyData.historyArray;
-    console.log("historyArray" + historyArray);
-    let lastPageVisited = historyData.lastPage;
-    console.log("lastPageVisited" + lastPageVisited);
-    setLastPage(lastPageVisited);
+  // const checkPage = () => {
+  //   console.log("checkpage");
+  //   console.log("history" + historyData);
+  //   console.log("history lenght" + historyData.historyArray.length);
+  //   console.log("inside checkpage if");
+  //   let historyArray = historyData.historyArray;
+  //   console.log("historyArray" + historyArray);
+  //   let lastPageVisited = historyData.lastPage;
+  //   console.log("lastPageVisited" + lastPageVisited);
+  //   setLastPage(lastPageVisited);
 
     // let lastPage = history[positionToRemove - 1];
     // setHistory(historyArray);
     // setPage(lastPage);
     
-  };
+  // };
   
-  const updateHistory = () => {
+  // const updateHistory = () => {
     
-    let historyList = historyData.historyArray;
-    let positionToRemove = historyList.length - 1;
-    console.log("positionToRemove" + positionToRemove);
-    let historyAfterGoBack = historyList.splice(positionToRemove, 1);
+  //   let historyList = historyData.historyArray;
+  //   let positionToRemove = historyList.length - 1;
+  //   console.log("positionToRemove" + positionToRemove);
+  //   let historyAfterGoBack = historyList.splice(positionToRemove, 1);
     
-    let newLastPage = historyList[historyList.length - 1];
-    console.log("newLastPage" + newLastPage);
+  //   let newLastPage = historyList[historyList.length - 1];
+  //   console.log("newLastPage" + newLastPage);
 
-    props.getHistory(historyAfterGoBack, newLastPage, props.page);
+  //   props.getHistory(historyAfterGoBack, newLastPage, props.page);
 
-  };
+  // };
 
   console.log("history" + historyData);
   // console.log("page" + page);
@@ -85,12 +93,19 @@ const Navbar = (props) => {
         <span className="app-subtitle">no Bixiga</span>
       </div>
       {
+        showBackButton && history.length > 0 && (
+          <button className={`navbar-btn ${showBackButton}`} onClick={() => history.goBack()}>
+            <RiArrowGoBackLine className="icon-menu icon" />
+          </button>
+        )
+      }
+      {/* {
         showBackButton && (
           <Link to={lastPage} className={`navbar-btn ${showBackButton}`} onClick={() => updateHistory()}>
             <RiArrowGoBackLine className="icon-menu icon" />
           </Link>
         )
-      }
+      } */}
     </div>
   )
 };
